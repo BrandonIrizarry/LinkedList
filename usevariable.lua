4af38bcd33db1d5ab2d9d7bde038b5ae159e5f7d
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2020-10-11 17:20:12 -0400

    Updated variable functionality to handle refreshes when a subroutine is entered.

--- usevariable.lua
+++ usevariable.lua
@@ -4,13 +4,20 @@ local V = require "variable"
 local list = L.create()
 local createVariable = V.initVariableSystem()
 
-list = list:cons(createVariable("x", "int", "field"))
-list = list:cons(createVariable("a", "Array", "static"))
-list = list:cons(createVariable("y", "int", "field"))
-list = list:cons(createVariable("s", "String", "static"))
+local function first (createVariable)
+	list = list:cons(createVariable("x", "int", "field"))
+	list = list:cons(createVariable("a", "Array", "static"))
+end
 
-list = list:cons(createVariable("Ax", "int", "argument"))
+local function second (createVariable)
+	list = list:cons(createVariable("y", "int", "field"))
+	list = list:cons(createVariable("s", "String", "static"))
+	list = list:cons(createVariable("Ax", "int", "argument"))
+end
+
+first(createVariable)
+second(createVariable)
 
 print(list)
 
-print(list:find(function (x) return x.name == "s" end))
+--print(list:find(function (x) return x.name == "s" end))
--- variable.lua
+++ variable.lua
@@ -1,41 +1,48 @@
 local M = {}
 
-function M.initVariableSystem ()
-	local system = {
-		field = -1,
-		static = -1,
-		argument = -1,
-		var = -1
-	}
+local system = {
+	field = -1,
+	static = -1,
+	argument = -1,
+	var = -1
+}
 
-	local methods = {}
-
-	function methods.__tostring (t)
-		return string.format("name=%s,datatype=%s,segment=%s,index=%d",
-			t.name, t.datatype, t.segment, t.index)
+function M.reset (...)
+	for i = 1, select("#", ...) do
+		local given = select(i, ...)
+		system[given] = -1
 	end
+end
 
-	-- create an item for the linked list/other storage
-	local function createVariable (name, datatype, segment)
-		system[segment] = system[segment] + 1
+local methods = {}
 
-		return setmetatable({
-			name = name,
-			datatype = datatype,
-			segment = segment,
-			index = system[segment]
-		}, methods)
-	end
+function methods.__tostring (t)
+	return string.format("name=%s,datatype=%s,segment=%s,index=%d",
+		t.name, t.datatype, t.segment, t.index)
+end
 
-	return createVariable
+function M.createVariable (name, datatype, segment)
+	system[segment] = system[segment] + 1
+
+	return setmetatable({
+		name = name,
+		datatype = datatype,
+		segment = segment,
+		index = system[segment]
+	}, methods)
 end
 
 --[[
-local createVariable = M.initVariableSystem()
-
-print(createVariable("x", "int", "field"))
-print(createVariable("y", "int", "field"))
-print(createVariable("a", "Array", "static"))
-print(createVariable("z", "String", "field"))
+print(M.createVariable("x", "int", "field"))
+print(M.createVariable("y", "int", "field"))
+print(M.createVariable("a", "Array", "static"))
+print(M.createVariable("z", "String", "field"))
+print(M.createVariable("i", "int", "var"))
+print(M.createVariable("j", "int", "var"))
+M.reset("argument", "var")
+print(M.createVariable("i", "int", "var"))
+print(M.createVariable("j", "int", "var"))
+print(M.createVariable("b", "Sprite", "static"))
 --]]
+
 return M
